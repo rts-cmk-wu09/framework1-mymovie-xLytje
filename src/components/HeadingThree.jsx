372cc2a7d224f3ecd00fe1a1c5039c0f6d85cfe8
xLytje <rasmuslytje@gmail.com>
2023-04-24 10:04:01 +0200

    h3 css

--- src/components/HeadingThree.jsx
+++ src/components/HeadingThree.jsx
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const Titel = styled.h3`
+  font-weight: 300;
+`;
+// font-weight: 200;
+
 const HeadingThree = () => {
-    return ( <h3>Titel</h3> );
+    return ( <Titel>Titel: Spiderman film</Titel> );
 }
  
 export default HeadingThree;
