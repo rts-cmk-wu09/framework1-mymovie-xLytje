aa64960dcad9e1b070699e4d4b7751529c099f42
xLytje <rasmuslytje@gmail.com>
2023-05-04 08:57:44 +0200

    pages and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
