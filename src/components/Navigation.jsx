98eada7e4928628445da50e2966edc93b7f4d470
xLytje <rasmuslytje@gmail.com>
2023-10-12 14:00:21 +0200

    update icons/links + darkmode

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { FaFilm, FaRegBookmark, FaTicketAlt } from "react-icons/fa";
+import { FaBookmark, FaHome, FaUserAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -15,18 +15,30 @@ const StyledNav = styled.nav`
   color: #979797;
   box-shadow: 0px 0px 5px 1px rgb(0 0 0 / 0.2);
 `;
-const StyledBookmark = styled(FaRegBookmark)`
+const StyledBookmark = styled(FaBookmark)`
+  font-size: 1.4rem;
+  color: #979797;
+`;
+const StyledHome = styled(FaHome)`
+  font-size: 1.8rem;
+  color: #979797;
+`;
+const StyledUser = styled(FaUserAlt)`
   font-size: 1.4rem;
   color: #979797;
 `;
 
 const Navigation = () => {
   return (
-    <StyledNav className="dark:bg-black">
-      <FaFilm />
-      <FaTicketAlt />
-      <Link to={`favorite`}>
+    <StyledNav className="dark:bg-black dark:shadow-3xl">
+      <Link to={`/`}>
+        <StyledHome />
+      </Link>
+      <Link to={`/favorite`}>
         <StyledBookmark />
+      </Link>
+      <Link to={`/`}>
+        <StyledUser />
       </Link>
     </StyledNav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0px 0px 5px 1px rgb(255 255 255 / 0.8);",
+      },
+    },
   },
   corePlugins: {
     preflight: false,
